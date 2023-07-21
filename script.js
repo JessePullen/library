1fca433b964cbda9bf70aacc10c41ccdbd4d6f31
Jesse Pullen <jessepullen@icloud.com>
2023-07-21 16:45:09 +1000

    Add popup for form with buttons for opening and closing

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const cardContent = document.querySelector('.card-content');
+const form = document.querySelector('.form');
+const addBookButton = document.querySelector('.add-book');
+const close = document.querySelector('.close');
 
-let myLibrary = ["The Hobbit", "Harry Potter"];
+addBookButton.addEventListener('click', () => {
+    form.style.display = 'block';
+});
+
+close.addEventListener('click', () => {
+    form.style.display = 'none';
+})
+
+// form.style.display = 'none';
+
+let myLibrary = [];
 
 
 
