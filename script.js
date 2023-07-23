54a56efca06f5205f32e84644416c54b6e144547
Jesse Pullen <jessepullen@icloud.com>
2023-07-23 19:27:36 +1000

    Pass through form data to script file

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const form = document.querySelector('.form');
-const addBookButton = document.querySelector('.add-book');
 const close = document.querySelector('.close');
+const submit = document.querySelector('.submit');
+const addBookButton = document.querySelector('.add-book');
+
+
+
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const pages = document.querySelector('#pages');
+    const read = document.querySelector('#read');
+
+    console.log(title.value, author.value, pages.value, read.value);
+
+})
+
+
 
 addBookButton.addEventListener('click', () => {
     form.style.display = 'block';
@@ -11,5 +28,18 @@ close.addEventListener('click', () => {
 })
 
 // form.style.display = 'none';
+
+// Constructor function for creating books
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+
+    myLibrary.push();
+}
 
 let myLibrary = [];
