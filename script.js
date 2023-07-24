07f1d706969ff0c7e66bdd89b5c90e5eb6a08746
Jesse Pullen <jessepullen@icloud.com>
2023-07-24 20:38:54 +1000

    Move code in submit button event listener to new function getFormData

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ const addBookButton = document.querySelector('.add-book');
 
 let myLibrary = [];
 
-// Gets form data and passes to constructor to create book
-submit.addEventListener('click', (e) => {
-    e.preventDefault(); // Stops submit button sending form to server
-
+// Gets form data and creates new book
+function getFormData() {
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const pages = document.querySelector('#pages').value;
     const read = document.querySelector('#read').checked;
 
-    const testBook = new Book(title, author, pages, read);
+    // Creates new book using constructor
+    const newBook = new Book(title, author, pages, read);
 
-    addBookToLibrary(testBook);
+    addBookToLibrary(newBook);
     console.log(myLibrary);
+}
+
+// Gets form data and passes to constructor to create book
+submit.addEventListener('click', (e) => {
+    e.preventDefault(); // Stops submit button sending form to server
+    getFormData();
 });
 
 // Show form pop-up
